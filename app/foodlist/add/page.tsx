f5b6f46ff57c16afa36add18e4ee88a4ce5ab140
sigunamu226 <sigunamu226@gmail.com>
2023-11-04 12:33:14 +0900

    レイアウト調整 -2

--- app/foodlist/add/page.tsx
+++ app/foodlist/add/page.tsx
@@ -15,10 +15,10 @@ export default function Page() {
   };
 
   return (
-    <div className="container ml-6 md:mx-auto mt-10">
+    <div className="container pl-6 md:mx-auto mt-10">
       <h1 className="text-3xl font-bold">食材登録</h1>
       <div className="mt-8">
-        <div className="w-6/12">
+        <div className="w-2/3 md:w-6/12">
           <Input
             className="[&>label]:text-lg [&>label]:font-bold"
             type="text"
@@ -29,7 +29,7 @@ export default function Page() {
             onChange={(e) => setFood({ ...food, name: e.target.value })}
           />
         </div>
-        <div className="mt-7 w-6/12">
+        <div className="mt-7 w-2/3 md:w-6/12">
           <Input
             className="[&>label]:text-lg [&>label]:font-bold"
             type="text"
@@ -40,7 +40,7 @@ export default function Page() {
             onChange={(e) => setFood({ ...food, rest_count: e.target.value })}
           />
         </div>
-        <div className="mt-7 w-6/12">
+        <div className="mt-7 w-2/3 md:w-6/12">
           <Input
             className="[&>label]:text-lg [&>label]:font-bold"
             type="date"
