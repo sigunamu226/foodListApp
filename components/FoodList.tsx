265f52b2822c2e126f25d13300a1ecc032db422f
sigunamu226 <sigunamu226@gmail.com>
2024-01-19 08:30:57 +0900

    一覧のCSSを調整、ページングを追加

--- components/FoodList.tsx
+++ components/FoodList.tsx
@@ -5,6 +5,7 @@ import { timestampStringToLocaleDateString } from "@/common/time";
 import { deleteFood } from "@/services/supabase";
 import {
   Button,
+  Pagination,
   Table,
   TableBody,
   TableCell,
@@ -14,7 +15,7 @@ import {
 } from "@nextui-org/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 interface IFoodListProps {
   foods: IFood[];
@@ -23,27 +24,52 @@ interface IFoodListProps {
 export const FoodList: React.FC<IFoodListProps> = (props) => {
   const router = useRouter();
   const [foods, setFoods] = useState<IFood[]>(props.foods);
+  const [page, setPage] = useState(1);
+
+  const rowsPerPage = 10;
+  const pages = Math.ceil(foods.length / rowsPerPage);
+
+  const items = useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+    return foods.slice(start, end);
+  }, [foods, page]);
 
   return (
-    <div className="mt-10">
-      <h1 className="text-3xl font-bold">食材在庫リスト</h1>
-      <div className="text-right">
+    <div className="my-10">
+      <h1 className="text-3xl font-bold ml-4 md:ml-0">食材在庫リスト</h1>
+      <div className="text-right mr-4 md:mr-0">
         <Link href="/foodlist/add">
           <Button>新規登録</Button>
         </Link>
       </div>
-      <div className="mt-10">
-        <Table aria-label="Example static collection table">
+      <div className="mt-10 mx-2 md:mx-0">
+        <Table
+          aria-label="Example static collection table"
+          bottomContent={
+            <div className="flex w-full justify-center">
+              <Pagination
+                isCompact
+                showControls
+                showShadow
+                color="secondary"
+                page={page}
+                total={pages}
+                onChange={(page) => setPage(page)}
+              />
+            </div>
+          }
+        >
           <TableHeader>
             {foodColumns.map((column, i) => (
               <TableColumn key={i}>{column}</TableColumn>
             ))}
           </TableHeader>
-          <TableBody>
-            {foods.map((food) => {
+          <TableBody items={items}>
+            {(food) => {
               return (
                 <TableRow key={food.id}>
-                  <TableCell>{food.name}</TableCell>
+                  <TableCell className="text-nowrap">{food.name}</TableCell>
                   <TableCell>{food.rest_count}</TableCell>
                   <TableCell>
                     {timestampStringToLocaleDateString(food.expiration_at)}
@@ -75,7 +101,7 @@ export const FoodList: React.FC<IFoodListProps> = (props) => {
                   </TableCell>
                 </TableRow>
               );
-            })}
+            }}
           </TableBody>
         </Table>
       </div>
